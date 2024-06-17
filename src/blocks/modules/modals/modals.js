b31185f897ca683d3057ec46d88ebb3a562c3d40
Ruslan Garapov <ruslangarapov1994@gmail.com>
2024-06-17 17:35:17 +0500

    Модальные окна для регистрации

--- src/blocks/modules/modals/modals.js
+++ src/blocks/modules/modals/modals.js
@@ -1,43 +1,63 @@
 const Modals = class Modals {
-    constructor({modalsSelector, modalsOpenerSelector, openedClass}){
-        this.modalsSelector = modalsSelector;
-        this.modalsOpenerSelector = modalsOpenerSelector;
-        this.openedClass = openedClass;
+  constructor({ modalsSelector, modalsOpenerSelector, openedClass }) {
+    this.modalsSelector = modalsSelector;
+    this.modalsOpenerSelector = modalsOpenerSelector;
+    this.openedClass = openedClass;
 
-        this.init();
+    this.init();
+  }
+  openModal(id) {
+    if (document.querySelector(`.modal.${this.openedClass}`)) {
+      document
+        .querySelectorAll(`.modal.${this.openedClass}`)
+        .forEach((modal) => {
+          modal.classList.remove(this.openedClass);
+        });
     }
-    openModal(id) {
-        if (!document.querySelector(`[${this.modalsSelector}="${id}"]`)) return;
-        document.querySelector(`[${this.modalsSelector}="${id}"]`).classList.add(this.openedClass);
-    }
-    closeModal(id) {
-        if (!document.querySelector(`[${this.modalsSelector}="${id}"]`)) return;
-        document.querySelector(`[${this.modalsSelector}="${id}"]`).classList.remove(this.openedClass);
-    }
-    addClickListener() {
-        document.addEventListener('click', (event) => {
-            if (event.target.dataset.modalId) {
-                event.preventDefault();
-                this.openModal(event.target.dataset.modalId);
-            }
-            if (!event.target.dataset.modalId && event.target.dataset.modal) {
-                event.preventDefault();
-                this.closeModal(document.querySelector(`[${this.modalsSelector}].isOpened`).dataset.modal);
-            }
-        })
-    }
-    addKeyupListener() {
-        document.addEventListener('keyup', (event) => {
-            if (event.keyCode === 27 && document.querySelector(`[${this.modalsSelector}].isOpened`)) {
-                this.closeModal(document.querySelector(`[${this.modalsSelector}].isOpened`).dataset.modal);
-            }
-        })
-    }
-    init() {
-        if (!this.modalsSelector && this.modalsOpenerSelector) return;
-        this.addClickListener();
-        this.addKeyupListener();
-    }
-}
+    if (!document.querySelector(`[${this.modalsSelector}="${id}"]`)) return;
+    document
+      .querySelector(`[${this.modalsSelector}="${id}"]`)
+      .classList.add(this.openedClass);
+  }
+  closeModal(id) {
+    if (!document.querySelector(`[${this.modalsSelector}="${id}"]`)) return;
+    document
+      .querySelector(`[${this.modalsSelector}="${id}"]`)
+      .classList.remove(this.openedClass);
+  }
+  addClickListener() {
+    document.addEventListener("click", (event) => {
+      if (event.target.dataset.modalId) {
+        event.preventDefault();
+        this.openModal(event.target.dataset.modalId);
+      }
+      if (!event.target.dataset.modalId && event.target.dataset.modal) {
+        event.preventDefault();
+        this.closeModal(
+          document.querySelector(`[${this.modalsSelector}].isOpened`).dataset
+            .modal,
+        );
+      }
+    });
+  }
+  addKeyupListener() {
+    document.addEventListener("keyup", (event) => {
+      if (
+        event.keyCode === 27 &&
+        document.querySelector(`[${this.modalsSelector}].isOpened`)
+      ) {
+        this.closeModal(
+          document.querySelector(`[${this.modalsSelector}].isOpened`).dataset
+            .modal,
+        );
+      }
+    });
+  }
+  init() {
+    if (!this.modalsSelector && this.modalsOpenerSelector) return;
+    this.addClickListener();
+    this.addKeyupListener();
+  }
+};
 
 export default Modals;
